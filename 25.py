af3301957b901ebcab83d997e8bbc3496abae456
H04ka <67304178+H04ka@users.noreply.github.com>
2024-12-10 18:10:25 +0500

    Delete 25.py

--- 25.py
+++
@@ -1,7 +0,0 @@
-import json
-
-with open('school.json', 'r') as dip:
-    data = json.load(dip)
-for classes in data["classes"]:
-    if classes['class_1'] > ['class_2']:
-        print(classes['students'])
